32e7c0bb1a1d65a6e9814fd5dc3dfabbcc274bc5
leejun7680 <44358654+leejun7680@users.noreply.github.com>
2018-12-10 21:20:11 +0900

    Add files via upload

--- Project Source/Sugang.java
+++ Project Source/Sugang.java
@@ -6,34 +6,68 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 
 public class Sugang extends JFrame {
-	JButton f1, f2, f3, f4, f5, f6;
-	JTextArea jt;
+	private JComboBox<String> Uf, Ut;
+	private JButton f1, f2, f3, f4, f5, select;
+	private JTextArea jt;
 	private ImageIcon im;
+	private String line;
+	private String a [] = {"교양", "전공"}, 
+				row [] = {"전공", "과목코드", "과목명", "학년", "구분", "학점", "수강인원", "시간표", "시수", "수강제한인원"},
+				hang [][] = new String[899][10];
+	private JTable Uhope,Uselect;
 	
 	public Sugang() {
 		setTitle("수강신청");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Color color = new Color(196,222,255);
 		
-		Container c2 = getContentPane();
-		c2.setBackground(color);
-		c2.setLayout(new FlowLayout());
-		
 		Container c = getContentPane();
 		c.setBackground(color);
-		c.setLayout(new FlowLayout());
+		c.setLayout(null);
 		
-	    jt = new JTextArea(0,0);
-	    
-		c2.add(jt);
+		Uf = new JComboBox<String>(a);
+		c.add(Uf);
+		Uf.setVisible(false);
+		Uf.setBounds(250, 62, 100, 30);
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("c:\\2018-2학기 시간표.txt"), "euc-kr"));
+			for(int i=0; i<900; i++) {
+				line = br.readLine();
+				if(line == null)
+					break;
+				String [] arr = line.split("\t");
+				for(int j=0; j<arr.length; j++)
+					hang[i][j] = arr[j];
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		
+		Uselect = new JTable(hang, row);
+		c.add(Uselect);
+		Uselect.setVisible(false);
+		Uselect.setBounds(0, 95, 675, 760);
+		
 		
 		JMenuBar m = new JMenuBar();
 		
@@ -45,11 +79,25 @@ public class Sugang extends JFrame {
 		
 		f1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-					
+				jt.setText("");
+				jt.setVisible(true);
+					try {
+						BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("c:\\사용자저장예비수강목록.txt"), "euc-kr"));
+						while (true) {
+							line = reader.readLine();
+							if (line == null)
+								break;
+							jt.setText(jt.getText() + line + "\n");
+						}
+						reader.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					select.setVisible(true);
 			}
 		});
 		
-		f2 = new JButton("직접입력");
+		f2 = new JButton("직접선택");
 		f2.setFont(new Font("Malgun Gothic Bold", Font.BOLD, 15));
 		f2.setBorderPainted(false);
 		f2.setContentAreaFilled(false);
@@ -57,11 +105,15 @@ public class Sugang extends JFrame {
 		
 		f2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-					
+				jt.setText("");
+				jt.setVisible(false);
+				select.setVisible(false);
+				Uf.setVisible(true);
+				Uselect.setVisible(true);
 			}
 		});
 		
-		f3 = new JButton("직접선택");
+		f3 = new JButton("사용자 신청 수강목록");
 		f3.setFont(new Font("Malgun Gothic Bold", Font.BOLD, 15));
 		f3.setBorderPainted(false);
 		f3.setContentAreaFilled(false);
@@ -73,7 +125,7 @@ public class Sugang extends JFrame {
 			}
 		});
 		
-		f4 = new JButton("사용자 신청 수강목록");
+		f4 = new JButton("시간표");
 		f4.setFont(new Font("Malgun Gothic Bold", Font.BOLD, 15));
 		f4.setBorderPainted(false);
 		f4.setContentAreaFilled(false);
@@ -85,7 +137,7 @@ public class Sugang extends JFrame {
 			}
 		});
 		
-		f5 = new JButton("시간표");
+		f5 = new JButton("뒤로가기");
 		f5.setFont(new Font("Malgun Gothic Bold", Font.BOLD, 15));
 		f5.setBorderPainted(false);
 		f5.setContentAreaFilled(false);
@@ -93,26 +145,28 @@ public class Sugang extends JFrame {
 		
 		f5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-					
-			}
-		});
-		
-		f6 = new JButton("뒤로가기");
-		f6.setFont(new Font("Malgun Gothic Bold", Font.BOLD, 15));
-		f6.setBorderPainted(false);
-		f6.setContentAreaFilled(false);
-		m.add(f6);
-		
-		f6.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
 				Main frame = new Main();
 				setVisible(false);
 			}
 		});
 		
 		c.add(m);
+		m.setBounds(5, 5, 670, 50);
 		
-		setSize(770, 800);
+		jt = new JTextArea();
+		c.add(jt);
+		jt.setVisible(false);
+		jt.setEditable(false);
+		jt.setBounds(0,60,670,700);
+		jt.setFont(new Font("Malgun Gothic Bold", Font.BOLD, 15));
+		
+		select = new JButton("전체신청");
+		c.add(select);
+		select.setVisible(false);
+		select.setBounds(240,770,100,50);
+		select.setFont(new Font("Malgun Gothic Bold", Font.BOLD, 15));
+		
+		setSize(680, 860);
 		setResizable(false);
 		setVisible(true);
 	}
