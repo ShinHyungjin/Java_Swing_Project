8fcebc99afd3809c056e6b247121d9d448e18d63
Lisonong2 <45355943+Lisonong2@users.noreply.github.com>
2018-12-10 21:19:04 +0900

    Update GongJi.java

--- Project Source/GongJi.java
+++ Project Source/GongJi.java
@@ -1,5 +1,7 @@
 package halla.Team4_Project;
 
+
+
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
@@ -22,21 +24,24 @@ public class GongJi extends JFrame {
 	public GongJi() {
 		setTitle("공지사항");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Color color1 = new Color(196,222,255);
+		Color color2 = new Color(54,88,156);
 
 		Container c = getContentPane();
-		c.setBackground(Color.ORANGE);
+		c.setBackground(color1);
 		c.setLayout(null);
 
 		text = new JLabel("공지사항");
 		c.add(text);
 		text.setFont(new Font("Malgun Gothic Bold",Font.BOLD,28));
-		text.setBounds(260, 1, 150, 50);
+		text.setBounds(290, 1, 150, 50);
+		
 		
 		area = new JTextArea(20, 30);
 		c.add(area);
-		area.setBackground(Color.ORANGE);
+		area.setBackground(Color.WHITE);
 		area.setFont(new Font("돋움",Font.BOLD,20));
-		area.setBounds(5, 60, 680, 660);
+		area.setBounds(7, 60, 680, 660);
 		area.setEditable(false);
 		
 		String line;
@@ -57,7 +62,10 @@ public class GongJi extends JFrame {
 		back = new JButton("뒤로가기");
 		c.add(back);
 		back.setFont(new Font("Malgun Gothic Bold",Font.BOLD,18));
-		back.setBounds(260, 730, 150, 50);
+		back.setBounds(270, 750, 150, 50);
+		back.setBackground(color2);
+		back.setForeground(Color.WHITE);
+		back.setFocusPainted(false);
 		
 		back.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
