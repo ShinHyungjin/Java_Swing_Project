b948ab821beacf39b95f3f42a5f97bfcfea5ae98
Lisonong2 <45355943+Lisonong2@users.noreply.github.com>
2018-12-11 14:23:49 +0900

    Update Credit3.java

--- Project Source/Credit3.java
+++ Project Source/Credit3.java
@@ -1,4 +1,4 @@
-package hoho.hoho;
+package lisonong;
 
 import java.awt.Color;
 import java.awt.Container;
@@ -16,7 +16,6 @@ import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -28,16 +27,17 @@ public class Credit3 extends JFrame {
 	JPanel panel;
 	static JComboBox<String> Jc, Um,Am;
 	static String [] a = {"부전공", "단일전공","복수전공"};
+	static String [] b = {"선택사항없음","경영학과", "관광경역학과","무역통상학과","광고영상미디어학과",
+			"사회복지학과","경찰행정학과","사회체육학과","뷰티헬스학과","기계자동차공학부","건축학과","토목공학과","산업경영공학과","신소재화학공학과","매카트로닉스공학과","전지전자공학과","정보통신소프트웨어학과","컴퓨터공학과"};
+	static String [] q = {"선택사항없음","경역학과","무역통상학과","광고영상미디어학과","전기전자공학과","건축학과","의료기기융합전공","벤처창업학전공","지식재산융합전공"};
 	JTextArea area;
 	String line;
-	JLabel w1;
-	JLabel w2;
-	JTextField King = null, KingKong = null;
-	JButton w;
+
 	public Credit3() {
 		setTitle("학점관리");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Color color = new Color(196,222,255);
+		Color color1 = new Color(54,88,156);
 		Container c = getContentPane();
 		c.setBackground(color);
 		c.setLayout(null);
@@ -70,16 +70,20 @@ public class Credit3 extends JFrame {
 			}
 		});
 		
-		Um = new JComboBox<String>();
+		Um = new JComboBox<String>(b);
 		c.add(Um);
 		Um.setBounds(450, 100, 380, 40);
 		Um.setAutoscrolls(true);
+		JComboBox<String> Um = new JComboBox<String>(b);
+		add(new JScrollPane(Um));
 			try {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("c:\\전공.txt"), "euc-kr"));
 				while (true) {
 					line = reader.readLine();
 					if (line == null)
 						break;
+					 area.append(line);
+	                    area.append("\n");
 					Um.addItem(line);
 				}
 				reader.close();
@@ -88,17 +92,18 @@ public class Credit3 extends JFrame {
 			}
 	
 	
-		Am = new JComboBox<String>();
+		Am = new JComboBox<String>(q);
 		c.add(Am);
 		Am.setBounds(450, 170, 380, 40);
 		Am.setAutoscrolls(true);
+		
 		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("c:\\전공.txt"), "euc-kr"));
 			while (true) {
 				line = reader.readLine();
 				if (line == null)
 					break;
-				Am.addItem(line);
+				Um.addItem(line);
 				}
 			reader.close();
 		} catch (IOException e1) {
@@ -111,22 +116,11 @@ public class Credit3 extends JFrame {
 			}
 		});
 		
-		
-
-		King = new JTextField();
-		King.setFont(new Font("휴먼고딕체",Font.BOLD,25));
-		c.add(King);
-		King.setEnabled(false);
-		King.setBounds(120, 100, 300, 40);
-		King.setHorizontalAlignment(SwingConstants.CENTER);
-
-
-		KingKong = new JTextField();
-		KingKong.setFont(new Font("휴먼고딕체",Font.BOLD,25));
-		c.add(KingKong);
-		KingKong.setEnabled(false);
-		KingKong.setBounds(120, 170, 300, 40);
-		KingKong.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel w1;
+		JLabel w2;
+		JTextField King;
+		JTextField KingKong;
+		JButton w;
 
 		area = new JTextArea();
 		@SuppressWarnings("unused")
@@ -140,30 +134,34 @@ public class Credit3 extends JFrame {
 		w = new JButton("확인");
 		w.setFont(new Font("휴먼고딕체",Font.BOLD,20));
 		c.add(w);
-		add(w);
 		w.setBounds(350, 240, 150, 40);
+		w.setBackground(color1);
+		w.setForeground(Color.WHITE);
 
 		w.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(((String)Um.getSelectedItem()).equals(Am.getSelectedItem())) {
-					JOptionPane.showMessageDialog(null, "제1전공과 부전공 학과가 같습니다.\n다시 선택하세요.", "전공선택오류", JOptionPane.WARNING_MESSAGE);
-					}
-				else
-					King.setText((String)Um.getSelectedItem());
-				KingKong.setText((String)Am.getSelectedItem());
-				try {
-					BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("c:\\부전공.txt"), "euc-kr"));
+				JFileChooser chooser = new JFileChooser();
+				int result = chooser.showOpenDialog(panel);	
+				if (result != JFileChooser.APPROVE_OPTION)
+					return;
+
+				String filePath = chooser.getSelectedFile().getPath();
+			try {
+					BufferedReader reader = new BufferedReader(new FileReader(filePath));
 					while (true) {
-						line = reader.readLine();
+						String line = reader.readLine();
 						if (line == null)
 							break;
-						area.append(line);
-	                    area.append("\n");
-											}
+						  area.append(line);
+		                    area.append("\n");
+						area.setText(area.getText() + line);
+					}
 					reader.close();
+
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
+
 			}
 		});
 
@@ -177,7 +175,20 @@ public class Credit3 extends JFrame {
 		c.add(w2);
 		w2.setBounds(10, 170, 140, 40);
 
-		
+		King = new JTextField("");
+		King.setFont(new Font("휴먼고딕체",Font.BOLD,25));
+		c.add(King);
+		King.setEnabled(false);
+		King.setBounds(120, 100, 300, 40);
+		King.setHorizontalAlignment(SwingConstants.CENTER);
+
+		KingKong = new JTextField("");
+		KingKong.setFont(new Font("휴먼고딕체",Font.BOLD,25));
+		c.add(KingKong);
+		KingKong.setEnabled(false);
+		KingKong.setBounds(120, 170, 300, 40);
+		KingKong.setHorizontalAlignment(SwingConstants.CENTER);
+
 		setSize(900, 900);
 		setResizable(false);
 		setVisible(true);
@@ -188,4 +199,3 @@ public class Credit3 extends JFrame {
 		Credit3 frame = new Credit3();
 	}
 }
-
